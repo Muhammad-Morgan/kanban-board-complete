74bf6fffa6c8c7b0af81e3c8d2d2117fc0f47057
Muhammad Morgan <muhammadmorgan589@gmail.com>
2026-02-28 02:16:43 +0200

    test deleteTask

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,17 +1,12 @@
 import { createTask } from "@/lib/create-task-service/createTaskService";
+import { deleteTask } from "@/lib/delete-task-service/deleteTaskService";
 import { editTask } from "@/lib/edit-task-service/editTaskService";
 import { BadRequestError, NotFoundError } from "@/lib/errors";
 import { NextResponse } from "next/server";
 
 export const POST = async (req: Request) => {
-  const { title, description, column, id } = await req.json();
-  const toBeEditTask = {
-    id,
-    title,
-    description,
-    column,
-  };
-  const resp = await editTask(toBeEditTask);
+  const { id } = await req.json();
+  const resp = await deleteTask({ id });
   if (!resp.success) return NextResponse.json({ msg: resp.message });
-  return NextResponse.json({ sucess: resp.success, task: resp.task });
+  return NextResponse.json({ sucess: resp.success });
 };
--- lib/delete-task-service/deleteTaskService.ts
+++ lib/delete-task-service/deleteTaskService.ts
@@ -1,5 +1,27 @@
 "use server";
+
+import { StatusCodes } from "http-status-codes";
+import dbConnect from "../dbConnect";
+import { Task, TaskType } from "@/models/Task/task";
+
 type DeleteButtonProps = {
   id: string;
 };
-export async function deleteTask({ id }: DeleteButtonProps) {}
+export async function deleteTask({ id }: DeleteButtonProps) {
+  if (!id)
+    return { message: "ID is missing", statusCode: StatusCodes.BAD_REQUEST };
+  await dbConnect();
+  // now let's delete
+  const taskGone = (await Task.findByIdAndDelete(id)) as
+    | TaskType
+    | null
+    | undefined;
+  if (!taskGone)
+    return {
+      message: `Couldn't find task with ID ${id}`,
+      statusCode: StatusCodes.NOT_FOUND,
+    };
+  return {
+    success: true,
+  };
+}
