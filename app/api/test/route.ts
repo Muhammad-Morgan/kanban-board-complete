cb88bc4c0342e85a6f19db115050bef5914b2657
Muhammad Morgan <muhammadmorgan589@gmail.com>
2026-02-28 02:08:12 +0200

    test editTask

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,11 +1,17 @@
 import { createTask } from "@/lib/create-task-service/createTaskService";
+import { editTask } from "@/lib/edit-task-service/editTaskService";
 import { BadRequestError, NotFoundError } from "@/lib/errors";
 import { NextResponse } from "next/server";
 
 export const POST = async (req: Request) => {
-  const { title, description, column } = await req.json();
-
-  const resp = await createTask({ title, description, column });
+  const { title, description, column, id } = await req.json();
+  const toBeEditTask = {
+    id,
+    title,
+    description,
+    column,
+  };
+  const resp = await editTask(toBeEditTask);
   if (!resp.success) return NextResponse.json({ msg: resp.message });
-  return NextResponse.json({ sucess: resp.success });
+  return NextResponse.json({ sucess: resp.success, task: resp.task });
 };
--- lib/edit-task-service/editTaskService.ts
+++ lib/edit-task-service/editTaskService.ts
@@ -1,8 +1,41 @@
 "use server";
+import { Task } from "@/models/Task/task";
+import { taskSchema } from "@/lib/zodSchemas";
+import dbConnect from "../dbConnect";
+import { StatusCodes } from "http-status-codes";
+
 type EditTaskProps = {
   id: string;
   title: string;
   description: string;
-  column: string;
+  column: "backlog" | "in-progress" | "review" | "done";
 };
-export const editTask = async (task: EditTaskProps) => {};
+export const editTask = async (task: EditTaskProps) => {
+  if (!task.title || !task.description || !task.column)
+    return {
+      message: "Please fill all the fields...",
+      statusCode: StatusCodes.BAD_REQUEST,
+    };
+  const validateFields = taskSchema.safeParse({
+    title: task.title,
+    description: task.description,
+    column: task.column,
+  });
+  if (!validateFields.success)
+    return {
+      message: validateFields.error.issues.map((i) => i.message).join(", "),
+      statusCode: StatusCodes.BAD_REQUEST,
+    };
+  // create the object in DB
+  await dbConnect();
+  const updatedTask = await Task.findByIdAndUpdate(
+    task.id,
+    validateFields.data,
+    { runValidators: true, new: true },
+  );
+  return {
+    task: updatedTask,
+    success: true,
+    statusCode: StatusCodes.OK,
+  };
+};
